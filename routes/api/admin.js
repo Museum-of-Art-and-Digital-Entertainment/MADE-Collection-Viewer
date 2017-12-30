560c480200009f58e75e3a016e78388c34b0365a
asajbel <serdan66@gmail.com>
2017-12-29 16:44:52 -0800

    stripped out logged in check from admin routes until the code is integrated and working

--- routes/api/admin.js
+++ routes/api/admin.js
@@ -17,36 +17,36 @@ router.get('/logout', function(req, res) {
 });
 
 /* GET admin home page. */
-router.get('/',isLoggedIn, controllers.admin.index);
+router.get('/', controllers.admin.index);
 
 
 /* GET all games with or without fuzzy search query*/
 /* Search Example /api/admin/games/?title=[title to search for]*/
 /* Or send query object with title paramater */
-router.get('/games', isLoggedIn, controllers.admin.getAllGames);
+router.get('/games', controllers.admin.getAllGames);
 
-router.get('/games/count', isLoggedIn,  controllers.admin.getGameCount);
+router.get('/games/count',  controllers.admin.getGameCount);
 
 /* GET all games with or without a strict name or id search*/
 router.get('/platforms', controllers.admin.getPlatforms);
 
 /* POST hit api and update database. */
-router.post('/game/updatedb', isLoggedIn, controllers.admin.updateDB);
+router.post('/game/updatedb', controllers.admin.updateDB);
 
 /* POST create a game */
-router.post('/games/create', isLoggedIn, controllers.admin.createGame);
+router.post('/games/create', controllers.admin.createGame);
 
 /* GET get a game to update/delete */
-router.get('/game/:id', isLoggedIn, controllers.admin.getGame);
+router.get('/game/:id', controllers.admin.getGame);
 
 /* GET the details on a game from thegamesdb.net api */
-router.get('/game/download/:id', isLoggedIn, controllers.admin.downloadGame);
+router.get('/game/download/:id', controllers.admin.downloadGame);
 
 /* POST update a game */
-router.put('/game/update/:id', isLoggedIn, controllers.admin.updateGame);
+router.put('/game/update/:id', controllers.admin.updateGame);
 
 /* POST delete a game */
-router.post('/game/:id/delete', isLoggedIn, controllers.admin.deleteGame);
+router.post('/game/:id/delete', controllers.admin.deleteGame);
 
 
 module.exports = router;
